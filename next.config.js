f0a5519928b20146aad453ba3f6d7a2282df0db9
Jose Firmino <jose.theangelz@gmail.com>
2021-08-07 11:29:10 -0300

    Next Img fix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+const withPlugins = require('next-compose-plugins')
+const withImages = require('next-images')
+
 module.exports = {
   reactStrictMode: true,
 }
+
+const nextConfig = {
+  images: {
+    domains: ['localhost']
+  }
+}
+
+module.exports = withPlugins([[withImages]], nextConfig)
